1c2619c1382f86642ecf7b1e7237aaf4dd7f8b25
Rion Dooley <dooley@tacc.utexas.edu>
2016-09-22 13:05:53 -0500

    APIDOCS * Updating the container init script to update the service url to the point to the public tenant

--- agave-apidocs/apidocs-api/src/main/docker/changehostname.sh
+++ agave-apidocs/apidocs-api/src/main/docker/changehostname.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sed -i 's#https://docker.example.com/docs/v2/resources#http://'$HOSTNAME'/docs/resources#' /var/www/html/docs/resources/index
+sed -i 's#https://docker.example.com/docs/v2/resources#http://'$(hostname)'/docs/resources#' /var/www/html/docs/resources/index
+for i in `ls /var/www/html/docs/resources/`
+do
+  sed -i 's#"https://docker.example.com"#"https://public.agaveapi.co"#g' /var/www/html/docs/resources/$i
+done
