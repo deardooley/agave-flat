5e1725c1b4b0703188d92b7b5edeaccfb8141dee
Richard Cardone <rcardone@tacc.utexas.edu>
2017-05-07 13:32:49 -0500

    AD-1344 Accomodate hidden parameter dependencies

--- agave-systems/systems-core/src/main/java/org/iplantc/service/systems/Settings.java
+++ agave-systems/systems-core/src/main/java/org/iplantc/service/systems/Settings.java
@@ -3,16 +3,12 @@
  */
 package org.iplantc.service.systems;
 
-import java.net.InetAddress;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.lang.StringUtils;
-import org.ietf.jgss.GSSCredential;
+import org.apache.log4j.Logger;
 
 /**
  * @author dooley
@@ -20,10 +16,9 @@ import org.ietf.jgss.GSSCredential;
  */
 public class Settings {
 
+	private static final Logger log = Logger.getLogger(Settings.class);
+	
 	private static Properties					props			= new Properties();
-
-	private static Map<String, GSSCredential>	userProxies		= Collections
-																		.synchronizedMap(new HashMap<String, GSSCredential>());
 
 	/* Trusted user settings */
 	public static List<String> 					TRUSTED_USERS = new ArrayList<String>();
@@ -59,7 +54,6 @@ public class Settings {
 	public static String						IPLANT_IO_SERVICE;
 	public static String						IPLANT_JOB_SERVICE;
 	public static String						IPLANT_PROFILE_SERVICE;
-//	public static String						IPLANT_ATMOSPHERE_SERVICE;
 	public static String						IPLANT_LOG_SERVICE;
 	public static String 						IPLANT_SYSTEM_SERVICE;
 	public static String 						IPLANT_METADATA_SERVICE;
@@ -70,7 +64,6 @@ public class Settings {
 
 
 	public static boolean						SLAVE_MODE;
-//	public static int							REFRESH_RATE	= 0;
 	
 	public static String						IRODS_USERNAME;
 	public static String						IRODS_PASSWORD;
@@ -79,7 +72,6 @@ public class Settings {
 	public static String						IRODS_ZONE;
 	public static String						IRODS_STAGING_DIRECTORY;
 	public static String						IRODS_DEFAULT_RESOURCE;
-//	public static int							IRODS_REFRESH_RATE;	// how often to check the irods connection info
 	public static String						PUBLIC_USER_USERNAME;
 	public static String						WORLD_USER_USERNAME;
 
@@ -95,108 +87,126 @@ public class Settings {
 		
 		HOSTNAME = org.iplantc.service.common.Settings.getLocalHostname();
 
-		API_VERSION = (String)props.getProperty("iplant.api.version");
+		API_VERSION = props.getProperty("iplant.api.version");
 		
-		SERVICE_VERSION = (String)props.getProperty("iplant.service.version");
+		SERVICE_VERSION = props.getProperty("iplant.service.version");
 		
-		COMMUNITY_USERNAME = (String) props.get("iplant.community.username");
+		COMMUNITY_USERNAME = props.getProperty("iplant.community.username");
 
-		COMMUNITY_PASSWORD = (String) props.get("iplant.community.password");
+		COMMUNITY_PASSWORD = props.getProperty("iplant.community.password");
 
-		String trustedUsers = (String)props.get("iplant.trusted.users");
+		String trustedUsers = props.getProperty("iplant.trusted.users");
 		if (!StringUtils.isEmpty(trustedUsers)) {
 			for (String user: trustedUsers.split(",")) {
 				TRUSTED_USERS.add(user);
 			}
 		}
 		
-		SERVICE_VERSION = (String)props.getProperty("iplant.service.version");
+		SERVICE_VERSION = props.getProperty("iplant.service.version");
 		
-		IPLANT_AUTH_SERVICE = (String)props.get("iplant.auth.service");
+		IPLANT_AUTH_SERVICE = props.getProperty("iplant.auth.service");
 		
-		IPLANT_MYPROXY_SERVER = (String) props.get("iplant.myproxy.server");
+		IPLANT_MYPROXY_SERVER = props.getProperty("iplant.myproxy.server");
 
-		IPLANT_MYPROXY_PORT = Integer.valueOf((String) props
-				.get("iplant.myproxy.port"));
+		try {IPLANT_MYPROXY_PORT = Integer.valueOf(props.getProperty("iplant.myproxy.port"));}
+			catch (Exception e) {
+				log.error("Failure loading setting iplant.myproxy.port", e);
+			}
 
-		IPLANT_LDAP_URL = (String) props.get("iplant.ldap.url");
+		IPLANT_LDAP_URL = props.getProperty("iplant.ldap.url");
 
-		IPLANT_LDAP_BASE_DN = (String) props.get("iplant.ldap.base.dn");
+		IPLANT_LDAP_BASE_DN = props.getProperty("iplant.ldap.base.dn");
 
-		TEMP_DIRECTORY = (String) props.get("iplant.server.temp.dir");
+		TEMP_DIRECTORY = props.getProperty("iplant.server.temp.dir");
 		
-		IPLANT_SYSTEM_SERVICE = (String) props.get("iplant.system.service");
-		if (!IPLANT_SYSTEM_SERVICE.endsWith("/")) IPLANT_SYSTEM_SERVICE += "/";
+		IPLANT_SYSTEM_SERVICE = props.getProperty("iplant.system.service");
+		if (IPLANT_SYSTEM_SERVICE != null)
+			if (!IPLANT_SYSTEM_SERVICE.endsWith("/")) IPLANT_SYSTEM_SERVICE += "/";
 		
-		IPLANT_IO_SERVICE = (String) props.get("iplant.io.service");
-		if (!IPLANT_IO_SERVICE.endsWith("/")) IPLANT_IO_SERVICE += "/";
+		IPLANT_IO_SERVICE = props.getProperty("iplant.io.service");
+		if (IPLANT_IO_SERVICE != null)
+			if (!IPLANT_IO_SERVICE.endsWith("/")) IPLANT_IO_SERVICE += "/";
 		
-		IPLANT_JOB_SERVICE = (String) props.get("iplant.job.service");
-		if (!IPLANT_JOB_SERVICE.endsWith("/")) IPLANT_JOB_SERVICE += "/";
+		IPLANT_JOB_SERVICE = props.getProperty("iplant.job.service");
+		if (IPLANT_JOB_SERVICE != null)
+			if (!IPLANT_JOB_SERVICE.endsWith("/")) IPLANT_JOB_SERVICE += "/";
 
-		IPLANT_PROFILE_SERVICE = (String) props.get("iplant.profile.service");
-		if (!IPLANT_PROFILE_SERVICE.endsWith("/")) IPLANT_PROFILE_SERVICE += "/";
-//
-//		IPLANT_ATMOSPHERE_SERVICE = (String) props.get("iplant.atmosphere.service");
-//		if (!IPLANT_ATMOSPHERE_SERVICE.endsWith("/")) IPLANT_ATMOSPHERE_SERVICE += "/";
+		IPLANT_PROFILE_SERVICE = props.getProperty("iplant.profile.service");
+		if (IPLANT_PROFILE_SERVICE != null)
+			if (!IPLANT_PROFILE_SERVICE.endsWith("/")) IPLANT_PROFILE_SERVICE += "/";
 		
-		IPLANT_LOG_SERVICE = (String) props.get("iplant.log.service");
-		if (!IPLANT_LOG_SERVICE.endsWith("/")) IPLANT_LOG_SERVICE += "/";
+		IPLANT_LOG_SERVICE = props.getProperty("iplant.log.service");
+		if (IPLANT_LOG_SERVICE != null)
+			if (!IPLANT_LOG_SERVICE.endsWith("/")) IPLANT_LOG_SERVICE += "/";
 		
-		IPLANT_METADATA_SERVICE = (String) props.get("iplant.metadata.service");
-		if (!IPLANT_METADATA_SERVICE.endsWith("/")) IPLANT_METADATA_SERVICE += "/";
+		IPLANT_METADATA_SERVICE = props.getProperty("iplant.metadata.service");
+		if (IPLANT_METADATA_SERVICE != null)
+			if (!IPLANT_METADATA_SERVICE.endsWith("/")) IPLANT_METADATA_SERVICE += "/";
 		
-		IPLANT_DOCS = (String) props.get("iplant.service.documentation");
-		if (!IPLANT_DOCS.endsWith("/")) IPLANT_DOCS += "/";
+		IPLANT_DOCS = props.getProperty("iplant.service.documentation");
+		if (IPLANT_DOCS != null)
+			if (!IPLANT_DOCS.endsWith("/")) IPLANT_DOCS += "/";
 		
-		DEBUG = Boolean.valueOf((String) props.get("iplant.debug.mode"));
+		try {DEBUG = Boolean.valueOf(props.getProperty("iplant.debug.mode", "false"));}
+			catch (Exception e) {
+        		log.error("Failure loading setting iplant.debug.mode.", e);
+        		DEBUG = false;
+			}
 
-		DEBUG_USERNAME = (String) props.get("iplant.debug.username");
-
+		DEBUG_USERNAME = props.getProperty("iplant.debug.username");
 		
-		KEYSTORE_PATH = (String) props.get("system.keystore.path");
+		KEYSTORE_PATH = props.getProperty("system.keystore.path");
 
-		TRUSTSTORE_PATH = (String) props.get("system.truststore.path");
+		TRUSTSTORE_PATH = props.getProperty("system.truststore.path");
 		
-		TRUSTED_CA_CERTS_DIRECTORY = (String) props.get("system.ca.certs.path");
+		TRUSTED_CA_CERTS_DIRECTORY = props.getProperty("system.ca.certs.path");
 		
-		MAIL_SERVER = (String) props.getProperty("mail.smtps.host");
+		MAIL_SERVER = props.getProperty("mail.smtps.host");
 		
-		MAILSMTPSPROTOCOL = (String) props.getProperty("mail.smtps.auth");
+		MAILSMTPSPROTOCOL = props.getProperty("mail.smtps.auth");
 		
-		MAILLOGIN = (String) props.getProperty("mail.smtps.user");
+		MAILLOGIN =  props.getProperty("mail.smtps.user");
 		
-		MAILPASSWORD = (String) props.getProperty("mail.smtps.passwd");
+		MAILPASSWORD = props.getProperty("mail.smtps.passwd");
 
-//		REFRESH_RATE = Integer.valueOf((String) props.get("iplant.refresh.interval"));
+		try {SLAVE_MODE = Boolean.valueOf(props.getProperty("iplant.slave.mode", "false"));}
+			catch (Exception e) {
+				log.error("Failure loading setting iplant.slave.mode.", e);
+				SLAVE_MODE = false;
+			}
 		
-		SLAVE_MODE = Boolean.valueOf((String) props.get("iplant.slave.mode"));
+		IRODS_USERNAME = props.getProperty("iplant.irods.username");
+
+		IRODS_PASSWORD =  props.getProperty("iplant.irods.password");
+
+		IRODS_HOST = props.getProperty("iplant.irods.host");
+
+		try {IRODS_PORT = Integer.valueOf((String) props.getProperty("iplant.irods.port"));}
+			catch (Exception e) {
+				log.error("Failure loading setting iplant.irods.port.", e);
+			}
+
+		IRODS_ZONE =  props.getProperty("iplant.irods.zone");
+
+		IRODS_STAGING_DIRECTORY = props.getProperty("iplant.irods.staging.directory");
+
+		IRODS_DEFAULT_RESOURCE = props.getProperty("iplant.irods.default.resource");
+
+		PUBLIC_USER_USERNAME = props.getProperty("iplant.public.user");
+
+		WORLD_USER_USERNAME = props.getProperty("iplant.world.user");
+
+		try {DEFAULT_PAGE_SIZE = Integer.parseInt((String) props.getProperty("iplant.default.page.size", "25"));}
+			catch (Exception e) {
+				log.error("Failure loading setting iplant.default.page.size.", e);
+				DEFAULT_PAGE_SIZE = 25;
+			}
 		
-		IRODS_USERNAME = (String) props.get("iplant.irods.username");
-
-		IRODS_PASSWORD = (String) props.get("iplant.irods.password");
-
-		IRODS_HOST = (String) props.get("iplant.irods.host");
-
-		IRODS_PORT = Integer.valueOf((String) props.get("iplant.irods.port"));
-
-		IRODS_ZONE = (String) props.get("iplant.irods.zone");
-
-		IRODS_STAGING_DIRECTORY = (String) props
-				.get("iplant.irods.staging.directory");
-
-		IRODS_DEFAULT_RESOURCE = (String) props
-				.get("iplant.irods.default.resource");
-
-//		IRODS_REFRESH_RATE = Integer.valueOf((String) props.get("iplant.irods.refresh.interval"));
-
-		PUBLIC_USER_USERNAME = (String) props.get("iplant.public.user");
-
-		WORLD_USER_USERNAME = (String) props.get("iplant.world.user");
-
-		DEFAULT_PAGE_SIZE = Integer.parseInt((String) props.getProperty("iplant.default.page.size", "25"));
-		
-		MAX_REMOTE_OPERATION_TIME = Integer.parseInt((String) props.getProperty("iplant.max.remote.connection.time", "90"));
+		try {MAX_REMOTE_OPERATION_TIME = Integer.parseInt(props.getProperty("iplant.max.remote.connection.time", "90"));}
+		catch (Exception e) {
+			log.error("Failure loading setting iplant.max.remote.connection.time.", e);
+			MAX_REMOTE_OPERATION_TIME = 90;
+		}
 	}
 
 }
